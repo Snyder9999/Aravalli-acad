e6cbe090a5e0722a4ba95d61911cdeb6f0123f58
Snyder <smhsuperhero9999@gmail.com>
2026-01-04 00:39:59 +0530

    Fix build errors: add missing auth constants

---
+++ src/constants/auth.ts
@@ -0,0 +1,9 @@
+export const DEFAULT_ROUTES_BY_ROLE = {
+    admin: '/admin',
+    user: '/',
+    SUPER_ADMIN: '/admin',
+    ADMIN: '/admin',
+    USER: '/',
+};
+
+export const authenticationRoute = ['/login', '/register'];
